adc9bd4e44a3ca981069c5247898ccb925fe0a59
berke-cura <berkecura@protonmail.com>
2022-04-07 19:43:18 +0300

    Updated: Search Inputs

--- src/components/searchWord/index.js
+++ src/components/searchWord/index.js
@@ -12,8 +12,14 @@ const SearchWord =  () => {
     });
 
     const handleChange = (event) => {
-        setSearchInfo({ ...searchInfo, [event.target.name]: event.target.value });
+        setSearchInfo({ ...searchInfo, [event.target.name]: (event.target.value)});
     };
+   
+    const handleChangeA = (event) => {
+      let { value, max } = event.target;
+      value = Math.max(Math.min(Number(max), Number(value)));
+      setSearchInfo({ ...searchInfo, [event.target.name]: value });
+  };
 
     const handleSubmit = (event) => {
         // prevents the submit button from refreshing the page
@@ -26,8 +32,12 @@ const SearchWord =  () => {
         const inputs=[];
         for(let i=0; i<value; i++){
           inputs.push(
-          <div><label>{(i+1) + ".Letter"}</label><input type="text" name={(i+1)} key={i+1}
-          onChange={handleChange} placeholder={(i+1) + ". Letter" }/></div>
+          <div><label>{(i+1) + ".Letter:"}</label><input 
+          type="text"
+          name={(i+1)}
+          onChange={handleChange}
+          maxLength="1"
+          /></div>
           )
         }
         return inputs;
@@ -35,36 +45,34 @@ const SearchWord =  () => {
     
     return (
         <Styled.Form>
-            <label>Letter Count</label>
-            <input
-            type="text"
+          <label>Letter Count:</label>
+          <input
+            type="number"
             name="letterCount"
-            placeholder="Letter Count"
             value={searchInfo.letterCount}
-            onChange={handleChange}
+            onChange={handleChangeA}
+            max="22"
           /><p/>
 
-            <label>Existing Letters</label>
+          <label>Existing Letters:</label>
           <input
             type="text"
             name="existLetters"
-            placeholder="Existing Letters"
-            value={searchInfo.existLetters}
+            placeholder="a,d,n"
+            value={(searchInfo.existLetters)}
             onChange={handleChange}
           /><p/>
 
-         <label>Not Existing Letters</label>
+          <label>Not Existing Letters:</label>
           <input
             type="text"
             name="notExistLet"
-            placeholder="Not Existing Letters"
-            value={searchInfo.notExistLet}
+            placeholder="c,b"
+            value={(searchInfo.notExistLet)}
             onChange={handleChange}
           />
-        <div>{renderInputs(searchInfo.letterCount)}</div>
-
-          <p/>
-            <button onClick={handleSubmit}>SEARCH</button>
+          <div>{renderInputs(searchInfo.letterCount)}</div>
+          <button onClick={handleSubmit}>SEARCH</button>
         </Styled.Form>
     )
 }
--- src/components/searchWord/style.js
+++ src/components/searchWord/style.js
@@ -3,11 +3,11 @@ import styled from 'styled-components'
 
 export const Form = styled.div`
   display: block;
-    color: black;
-    font-size: 10px;
+  color: black;
+  font-size: 20px;
+  font-weight: bold;
 
     &:focus {
-  outline: none;
 }
 
 `
