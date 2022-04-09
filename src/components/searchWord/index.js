9a86e535044f84649c2678c7af111ce65476b800
Süleyman Ali Akpınar <suleyman@linkadoo.me>
2022-04-09 22:29:41 +0300

    merge

--- src/components/searchWord/index.js
+++ src/components/searchWord/index.js
@@ -1,97 +1,114 @@
 import * as Styled from './style'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import RICIBs from 'react-individual-character-input-boxes';
 import { SectionContainer } from '../../components'
 
-
-
 const SearchWord =  () => {
-    const [searchInfo, setSearchInfo] = useState({
-        letterCount: "",
-        existLetters: "",
-        notExistLet: "",
-    });
+    const [letterCount, setLetterCount] = useState(1)
+    const [inputProps, setInputProps] = useState([])
+    const [existLetters, setExistLetters] = useState([])
+    const [notExistLet, setNotExistLet] = useState([])
+    const [placementLetters, setPlacementLetters] = useState([])
 
-    const handleChange = (event) => {
-        setSearchInfo({ ...searchInfo, [event.target.name]: (event.target.value)});
-    };
-   
-    const handleChangeA = (event) => {
-      let { value, max } = event.target;
-      value = Math.max(Math.min(Number(max), Number(value)));
-      setSearchInfo({ ...searchInfo, [event.target.name]: value });
-  };
+    const inputStyle = {
+      "padding": "13px 20px",
+      "height": "32px",
+      "alignItems": "center",
+      "textAlign": "center",
+      "justifyContent": "center",
+      "width": "30px",
+      "fontWeight": "500",
+      "borderRadius": "4px",
+      "fontSize": "18px",
+      "lineHeight": "28px",
+      "letterSpacing": "0.5px",
+      "outline": "none",
+      "color": "#c4c3ca",
+      "backgroundColor": "#1f2029",
+      "border": "none",
+      "WebkitTransition": "all 200ms linear",
+      "transition": "all 200ms linear",
+      "boxShadow": "0 4px 8px 0 rgba(21,21,21,.2)",
+    }
 
     const handleSubmit = (event) => {
         // prevents the submit button from refreshing the page
         event.preventDefault();
-        console.log(searchInfo); 
+        //console.log(searchInfo); 
       };
 
 
-    function renderInputs(value){
-        const inputs=[];
-        for(let i=0; i<value; i++){
-          inputs.push(
-          <Styled.LetterBoxes>
-          <Styled.LetterInput className='LetterBox'
-            size='5'
-            type="text"
-            name={(i+1)}
-            onChange={handleChange}
-            maxLength="1"
-            placeholder={(i+1) + "."}
-          />
-          </Styled.LetterBoxes>
-          )
+       const handlePlacement = (string) => {
+        console.log(string)
+        //console.log(string); 
+      };
+      const onChangeHandle = (event) => {
+        const count = event.target.value;
+        setLetterCount(parseInt(count))
+      }
+      
+      useEffect(()=> {
+        if(letterCount < 1 ) {
+          setInputProps([])
         }
-        return inputs;
-       }
-    
+        else{
+        const dummy  = Array(letterCount).fill({style: inputStyle})
+        var r = dummy.map((item, i) => {
+          return { style: inputStyle}
+        })
+        
+         setInputProps(r)
+        }
+      }, [letterCount])
+
+
     return (
         <Styled.Container>
 
           <Styled.InputArea>
 
               <Styled.InputContainer>
-                    <Styled.Label1>Letter Count:</Styled.Label1>
-                    <Styled.Input1
-                       type="number"
-                       name="letterCount"
-                       value={searchInfo.letterCount}
-                       onChange={handleChangeA}
-                       max="22"
-                     />
-              </Styled.InputContainer>
+              <Styled.Label1>Letter Count</Styled.Label1>
+                        <Styled.Group>
+                          <Styled.CounterButton onClick={()=> setLetterCount(letterCount+1)}>+</Styled.CounterButton>
+                                <Styled.InputSulo min="1" max="22" type="number" name="letterCount" placeholder="1" id="logemail" autoComplete="off" value={letterCount} onChange={(event => onChangeHandle(event))} />
+                          <Styled.CounterButton onClick={()=> setLetterCount(letterCount-1)}>-</Styled.CounterButton>
 
+                        </Styled.Group>
+              </Styled.InputContainer>
               <Styled.InputContainer>
-                   <Styled.Label2>Existing Letters:</Styled.Label2>
-                   <Styled.Input2
-                     type="text"
-                     name="existLetters"
-                     placeholder="a,d,n"
-                     value={(searchInfo.existLetters)}
-                     onChange={handleChange}
+                    <Styled.Label1>Placement Letters:</Styled.Label1>
+                    <Styled.Group>
+                    <RICIBs
+                     amount={letterCount === 0 ? 1 : letterCount}
+                     autoFocus
+                     inputProps={inputProps}
+                     handleOutputString={handlePlacement}
+                     inputRegExp={/^[A-Za-z][A-Za-z0-9]*$/}
                    />
+                  </Styled.Group>
+                
+                </Styled.InputContainer>
+                   
+              <Styled.InputContainer>
+                   <Styled.Label1>Existing Letters</Styled.Label1>
+                   <Styled.Group>
+                   <Styled.InputLetters type="text" name="letterCount" placeholder="a,b,c,d,e" id="logemail" autoComplete="off"/>
+                  </Styled.Group>
+                   
               </Styled.InputContainer>
           
               <Styled.InputContainer>
-                    <Styled.Label3>Not Existing Letters:</Styled.Label3>
-                    <Styled.Input3
-                      type="text"
-                      name="notExistLet"
-                      placeholder="c,b"
-                      value={(searchInfo.notExistLet)}
-                      onChange={handleChange}
-                    />
+                    <Styled.Label1>Not Existing Letters:</Styled.Label1>
+                    <Styled.Group>
+                   <Styled.InputLetters type="text" name="letterCount" placeholder="a,b,c,d,e" id="logemail" autoComplete="off"/>
+                  </Styled.Group>
+                  
                 </Styled.InputContainer>
+               
 
           </Styled.InputArea>
 
-          <Styled.ResultContainer>         
-               <Styled.LetterBoxes>
-                 <div>{renderInputs(searchInfo.letterCount)}</div>
-               </Styled.LetterBoxes>
-          </Styled.ResultContainer>
 
           <Styled.SearchButton onClick={handleSubmit}>SEARCH</Styled.SearchButton>
         </Styled.Container>
--- src/components/searchWord/style.js
+++ src/components/searchWord/style.js
@@ -91,9 +91,12 @@ border-radius: 5px;
 export const Label1 = styled.label`
 font-size: 20px;
 font-weight: bolder;
-margin-left: 5%;
 margin-bottom: 10px;
-
+flex: 1;
+display: flex;
+width: 100%;
+text-align: center;
+justify-content: center;
 `
 
 
@@ -140,5 +143,88 @@ margin-bottom: 10px;
 `
 
 export const InputContainer = styled.div`
-        
+flex-direction: column;
+display:flex;
+padding: 10px;
+`
+
+export const Group = styled.div`
+flex-direction: row;
+display:flex;
+justify-content: center;
+padding: 1rem;
+`
+export const InputSulo = styled.input`
+  padding: 13px 20px;
+  height: 32px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  width: 10%;
+  font-weight: 500;
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  letter-spacing: 0.5px;
+  outline: none;
+  color: #c4c3ca;
+  background-color: #1f2029;
+  border: none;
+  -webkit-transition: all 200ms linear;
+  transition: all 200ms linear;
+  box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
+  ::-webkit-inner-spin-button{
+    -webkit-appearance: none; 
+    margin: 0; 
+}
+::-webkit-outer-spin-button{
+    -webkit-appearance: none; 
+    margin: 0; 
+}  
+`
+
+export const CounterButton = styled.button`
+display: flex;
+margin-left: 0.2rem;
+margin-right: 0.2rem;
+height: 58px;
+align-items: center;
+text-align: center;
+justify-content: center;
+width: 15%;
+font-weight: 500;
+border-radius: 4px;
+font-size: 18px;
+line-height: 28px;
+letter-spacing: 0.5px;
+outline: none;
+color: #c4c3ca;
+background-color: #343645;
+border: none;
+transition: all 200ms linear;
+box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
+&:hover {
+  background-color: #3f4254;
+};
+`
+
+export const InputLetters = styled.input`
+  padding: 13px 20px;
+  height: 32px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  width: 70%;
+  font-weight: 500;
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  letter-spacing: 0.5px;
+  outline: none;
+  color: #c4c3ca;
+  background-color: #1f2029;
+  border: none;
+  -webkit-transition: all 200ms linear;
+  transition: all 200ms linear;
+  box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
 `
