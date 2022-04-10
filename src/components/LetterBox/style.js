ea7f357ada68d51be5146ca3ee5c3c4c46c24185
berke-cura <berkecura@protonmail.com>
2022-04-10 16:34:11 +0300

    Homepage css updated

--- src/components/LetterBox/style.js
+++ src/components/LetterBox/style.js
@@ -1,31 +1,10 @@
 import styled from 'styled-components'
 
 export const LetterBox = styled.input`
-  padding: 13px 20px;
-  height: 22px;
-  align-items: center;
   text-align: center;
-  justify-content: center;
-  margin:1px;
-  width: 4%;
-  font-weight: 500;
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  letter-spacing: 0.5px;
-  outline: none;
-  color: #c4c3ca;
-  background-color: #1f2029;
-  border: none;
-  -webkit-transition: all 200ms linear;
-  transition: all 200ms linear;
-  box-shadow: 0 4px 8px 0 rgba(21,21,21,.2);
-  ::-webkit-inner-spin-button{
-    -webkit-appearance: none; 
-    margin: 0; 
-}
-::-webkit-outer-spin-button{
-    -webkit-appearance: none; 
-    margin: 0; 
-}  
+  margin:10px;
+  border: 1px solid black;
+  width: 30px;
+  height: 30px;
+
 `
